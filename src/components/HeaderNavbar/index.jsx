ed1b7405549b1e00b919a41b74e9143b0840745e
chinguyen98 <dacchi6761@gmail.com>
2020-11-04 13:20:50 +0700

    update

--- src/components/HeaderNavbar/index.jsx
+++ src/components/HeaderNavbar/index.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { useState } from 'react';
+import { Collapse, Container, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Row, UncontrolledDropdown } from 'reactstrap';
 
 import './header-navbar.scss';
 import brand_logo from '../../assets/images/brand_logo.png'
 
 function HeaderNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div>
-      <Navbar className="header-navbar">
-        <NavbarBrand>
-          <img src={brand_logo} alt="brand_logo" />
-        </NavbarBrand>
+    <>
+      <Navbar className="header-navbar" light expand="sm">
+        <Container>
+          <Row>
+            <NavbarBrand className="pl-4">
+              <img src={brand_logo} alt="brand_logo" />
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="#">Home</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Pages
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>
+                      Service
+                  </DropdownItem>
+                    <DropdownItem>
+                      Personal
+                  </DropdownItem>
+                    <DropdownItem>
+                      Q&A
+                  </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Row>
+        </Container>
       </Navbar>
-    </div>
+    </>
   )
 }
 
--- src/components/SocialArea/index.jsx
+++ src/components/SocialArea/index.jsx
@@ -10,11 +10,17 @@ function SocialArea() {
   return (
     <Container className="social-area">
       <Row>
-        <Col lg="6" sm="12">
-          <span className="mx-1">"Phone: +1800-222-3333"</span>
-          <span className="mx-1">"Email: contact@gmail.com"</span>
+        <Col lg="7" sm="12">
+          <Row>
+            <Col lg="6" sm="12">
+              <span className="mx-1">"Phone: +1800-222-3333"</span>
+            </Col>
+            <Col lg="6" sm="12">
+              <span className="mx-1">"Email: contact@gmail.com"</span>
+            </Col>
+          </Row>
         </Col>
-        <Col lg="6" sm="12" className="social-area__icon">
+        <Col lg="5" sm="12" className="social-area__icon">
           <FontAwesomeIcon icon={faCoffee} />
           <FontAwesomeIcon icon={faLinkedin} />
           <FontAwesomeIcon icon={faTwitter} />
